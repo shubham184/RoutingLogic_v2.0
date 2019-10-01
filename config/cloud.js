30d03a8c53f2469e660298f820b9156459eec5fc
Marco Littel <marco.littel@ts.fujitsu.com>
2019-10-01 11:54:47 +0200

    add  cloud config

---
+++ config/cloud.js
@@ -0,0 +1,16 @@
+/* eslint linebreak-style: ["error", "windows"] */
+module.exports = {
+  redisURL: "GlobalSTORE1@redis-11736.c8.us-east-1-3.ec2.cloud.redislabs.com:11736",
+  botAPIEndPoint: "https://api.cai.tools.sap:443/build/v1/dialog",
+  botConnector: "http://localhost:8082/v1/",
+  livechatConnector: "http://localhost:8084/",
+  livechatEndpoint: "livechatMessage",
+  agentAvailability: "agentavailable",
+  connectorId: "0291ca68-e297-4774-8e28-130243d60e19",
+  defaultBotToken: "5f495d931aaaff155657eea874ff5cd7",
+  port: "8087",
+  https: false,
+  keyfile: "gmclouddemo.westeurope.cloudapp.azure.com-key.pem",
+  certfile: "gmclouddemo.westeurope.cloudapp.azure.com-chain.pem",
+  logfileLocation: "routinglogic.log",
+};
