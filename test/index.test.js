41b1d81603b40e4bbc82b6e01a335b16fde92de8
Littel <marco.littel@ts.fujitsu.com>
2019-10-16 11:10:56 +0200

    bot connector call unittest

--- test/index.test.js
+++ test/index.test.js
@@ -7,6 +7,7 @@ const nock = require("nock");
 const uuidv4 = require("uuid/v4");
 chai.use(require("chai-http"));
 
+const config = require("../config");
 const app = require("../src/index");
 
 const { expect } = chai;
@@ -14,15 +15,15 @@ const { expect } = chai;
 describe("post GET to /", () => {
   it("should retrieve a hello world message", (done) => {
     const endpoint = "/";
-    
+
     chai.request(app)
       .get(endpoint)
       .then((res) => {
         expect(res.status).to.equal(200);
         done();
       });
-  } 
-  )});
+  });
+});
 
 describe("post hi to SAP testchatbot", () => {
   it("should connect to SAP with simple message and receive a response 200 even if SAP is not available", (done) => {
@@ -51,3 +52,36 @@ describe("post hi to SAP testchatbot", () => {
   }).timeout(150000);
 });
 
+describe("post agentmessage into a conversation", () => {
+  it("should return status 200 when message has been posted into the bot connector", (done) => {
+    // we will nock the bot connector endpoint, based on the conversation ID
+    const conversationId = uuidv4();
+    const endpoint = "/agentmessage";
+
+    const myReply = {
+
+    };
+
+    const bcUrl = `${config.botConnector}`;
+    const bcPath = `/connectors/${config.connectorId}/conversations/${conversationId}/messages`;
+
+    nock(bcUrl)
+      .post(bcPath)
+      .reply(200, myReply);
+
+    const myRequest = {
+      conversation_id: conversationId,
+      agentName: "test agent",
+      message: "my message from a test agent",
+    };
+
+    chai.request(app)
+      .post(endpoint)
+      .send(myRequest)
+      .then((res) => {
+        expect(res.status).to.equal(201);
+        expect(res.text).to.equal(`agentmessage posted to http:/localhost:8082/v1//connectors/${config.connectorId}/conversations/${conversationId}/messages`);
+        done();
+      });
+  });
+});
